369e697fc9f386085b3d8e71a24f976533b6516d
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-02-19 23:48:42 +0530

    About PCOS 4

--- src/app/About-PCOS/page.jsx
+++ src/app/About-PCOS/page.jsx
@@ -38,7 +38,55 @@ const About= () => {
         <li>Weight gain</li>
       </ul>
 
-      
+      <h2>What Causes PCOS</h2>
+      <p>
+        PCOS can result from elevated androgen levels, leading to the accumulation of abdominal and visceral fat, fostering insulin resistance, and amplifying androgen production, fueling the progression of PCOS and its symptoms.
+      </p>
+
+      <h2>Challenges Faced by Individuals with PCOS</h2>
+      <ul>
+        <li>Infertility</li>
+        <li>Obesity</li>
+        <li>Type 2 diabetes</li>
+        <li>Cardiovascular diseases</li>
+        <li>Miscarriages</li>
+        <li>Breast cancer</li>
+        <li>Anxiety</li>
+        <li>Eating disorders</li>
+      </ul>
+
+      <h2>In-depth Exploration of How PCOS Can Impact Fertility</h2>
+      <p>
+        PCOS, identified by the World Health Organization as a primary cause of anovulation, significantly contributes to infertility, affecting 70-80% of women with anovulatory infertility. The intricate nature of PCOS, marked by challenges in diagnosis, particularly highlights infertility as a distinctive complication linked to ovarian development.
+      </p>
+
+      <p>
+        In a normal ovary, the follicle cycle completes, but in polycystic ovaries, follicle development halts, resulting in multiple immature follicles and contributing to infertility. Hormonal imbalances in PCOS disrupt regular ovulation, causing irregular menstruation, anovulation, and lower-quality eggs, hindering conception.
+      </p>
+
+      <p>
+        PCOS-related disorders like insulin resistance and androgen excess further exacerbate infertility issues in affected individuals globally.
+      </p>
+
+      <h2>Statistical Information on Fertility Rates for Individuals with PCOS</h2>
+      <ul>
+        <li>7 out of 10 individuals with PCOS are affected by anovulatory infertility</li>
+        <li>4.5 times higher likelihood of infertility in women with PCOS.</li>
+        <li>70% of cases among 25% of couples experiencing fertility challenges are due to PCOS</li>
+      </ul>
+
+      <h2>The Importance of Early Diagnosis</h2>
+      <p>
+        Pre-treatment risk assessment is vital to prevent negative outcomes, and recognizing the higher risk allows for implementing preventive measures to mitigate potential severe outcomes such as spontaneous abortion, the risk of miscarriage, etc.
+      </p>
+
+      <p>
+        Thus, analyzing and understanding a PCOS patient's risk of becoming infertile would directly impact the individual’s fertility and reproductive health with relation to PCOS.
+      </p>
+
+      <p>
+        Therefore, communicating the risk regarding these possible effects and where appropriate, prevention methods and treatment, should dominate the conversation between the clinician and the patient.
+      </p>
 
    </div>
 
