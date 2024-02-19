c2921db1093c9ffe13066b54e9959e2d2bbbe788
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-02-19 23:41:52 +0530

    About PCOS 2

--- src/app/About-PCOS/page.jsx
+++ src/app/About-PCOS/page.jsx
@@ -10,6 +10,16 @@ const About= () => {
         PCOS, or Polycystic Ovary Syndrome, is a prevalent hormonal condition affecting women in their late teens or early twenties. It is characterized by hyperandrogenism (elevated male hormones causing ovarian cysts), anovulation (irregular or absent ovulation), and the presence of multiple fluid-filled sacs on the ovaries.
       </p>
 
+      <h2>Types of PCOS</h2>
+      <ol>
+        <li>Insulin-resistant PCOS - Elevated insulin levels.</li>
+        <li>Inflammatory PCOS - Chronic inflammation exacerbating symptoms.</li>
+        <li>Hidden-cause PCOS - Triggered by stress, environment, or genetics.</li>
+        <li>Pill-induced PCOS - Resulting from progestin in birth control pills, disrupting hormone balance.</li>
+      </ol>  
+
+      
+
    </div>
 
     
