36befcba1081ecb6b9fed3b4560f1cb488a71e0c
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-02-19 23:36:03 +0530

    About PCOS 1

--- src/app/About-PCOS/page.jsx
+++ src/app/About-PCOS/page.jsx
@@ -5,6 +5,11 @@ const About= () => {
   return (
     
    <div>
+    <h1>PCOS - Polycystic Ovary Syndrome</h1>
+      <p>
+        PCOS, or Polycystic Ovary Syndrome, is a prevalent hormonal condition affecting women in their late teens or early twenties. It is characterized by hyperandrogenism (elevated male hormones causing ovarian cysts), anovulation (irregular or absent ovulation), and the presence of multiple fluid-filled sacs on the ovaries.
+      </p>
+
    </div>
 
     
