d27aa53e65312cdcb8262f9bb35398022430b706
Ranuga Anthony <ranuga.20220298@iit.ac.lk>
2024-02-25 13:16:30 +0530

    made changes to app.module.ts connecting the backend with the mongodb database using mongoose

--- pcos-backend/src/app.module.ts
+++ pcos-backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://PCOSFertilicareDB:6OjUyaiYp4X8365d@cluster0.zpgsbrp.mongodb.net/PCOSFerilicare?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
-})
-export class AppModule {}
+});
+export class AppModule {};
