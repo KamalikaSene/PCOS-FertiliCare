3f8da1dd5d4ae5133ec645393f2e1849093758f0
Ranuga Anthony <ranuga.20220298@iit.ac.lk>
2024-02-25 14:32:01 +0530

    committing changes to doctor.controller.ts and doctor.service.ts

--- pcos-backend/src/doctors/doctors.controller.ts
+++ pcos-backend/src/doctors/doctors.controller.ts
@@ -1,4 +1,17 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('doctors')
-export class DoctorsController {}
+export class DoctorsController {
+    @Get()
+    findAll().{
+        return []
+    }
+
+    @Get(":id")
+    findOne().{
+        return[];
+    }
+};
+
+
+// controller file to handle all the routes in respect to doctors.
--- pcos-backend/src/doctors/doctors.service.ts
+++ pcos-backend/src/doctors/doctors.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class DoctorsService {}
+@Injectable();
+export class DoctorsService {};
+
+// service file to handle all the methods for all the http requests being made to the backend which are then handled by the particular route in the controller file
