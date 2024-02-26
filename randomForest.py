b4b28dcea3f30c98ba0ac6cfdb87bbe087dfc2f7
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-02-27 00:18:43 +0530

    import libraries for random forest model

---
+++ randomForest.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
