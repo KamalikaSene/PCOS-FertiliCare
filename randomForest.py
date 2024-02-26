f3885776924c4fb4cd481aa0841093f281ba62be
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-02-27 00:19:19 +0530

    loaded the data set

--- randomForest.py
+++ randomForest.py
@@ -4,3 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 
+# loaded the data set
+df = pd.read_excel("PCOS_data_without_infertility.xlsx",sheet_name=1)
+df.head()
