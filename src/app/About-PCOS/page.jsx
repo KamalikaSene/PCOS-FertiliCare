c07ef20cb1c00a7966d10b3d4295efeabda24d9a
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-02-28 00:53:47 +0530

    New commits on Styles

--- src/app/About-PCOS/page.jsx
+++ src/app/About-PCOS/page.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
+import styles from "./about.module.css"
 
 
 const About= () => {
   return (
     
    <div>
-    <h1>PCOS - Polycystic Ovary Syndrome</h1>
-      <p>
+    <h1 className={styles.h1}>PCOS - Polycystic Ovary Syndrome</h1>
+      <p className={styles.p}>
         PCOS, or Polycystic Ovary Syndrome, is a prevalent hormonal condition affecting women in their late teens or early twenties. It is characterized by hyperandrogenism (elevated male hormones causing ovarian cysts), anovulation (irregular or absent ovulation), and the presence of multiple fluid-filled sacs on the ovaries.
       </p>
 
-      <h2>Types of PCOS</h2>
-      <ol>
+      <h2 className={styles.h2}>Types of PCOS</h2>
+      <ol className={styles.ol}>
         <li>Insulin-resistant PCOS - Elevated insulin levels.</li>
         <li>Inflammatory PCOS - Chronic inflammation exacerbating symptoms.</li>
         <li>Hidden-cause PCOS - Triggered by stress, environment, or genetics.</li>
         <li>Pill-induced PCOS - Resulting from progestin in birth control pills, disrupting hormone balance.</li>
       </ol>  
 
-      <h2>PCOS Affects</h2>
+      <h2 className={styles.h2}>PCOS Affects</h2>
       <ul>
         <li>1 in 12 individuals</li>
         <li>4-8% individuals of childbearing age globally</li>
         <li>31.3% Asian individuals</li>
       </ul>
 
-      <p>
+      <p className={styles.p}>
          Despite impacting reproductive, metabolic, and psychological well-being, PCOS is often underdiagnosed due to its diverse symptoms. These vary among women, shaped by individual differences in the environment, behavior, lifestyle, and genetics.
       </p>
 
@@ -38,12 +39,12 @@ const About= () => {
         <li>Weight gain</li>
       </ul>
 
-      <h2>What Causes PCOS</h2>
-      <p>
+      <h2 className={styles.h2}>What Causes PCOS</h2>
+      <p className={styles.p}>
         PCOS can result from elevated androgen levels, leading to the accumulation of abdominal and visceral fat, fostering insulin resistance, and amplifying androgen production, fueling the progression of PCOS and its symptoms.
       </p>
 
-      <h2>Challenges Faced by Individuals with PCOS</h2>
+      <h2 className={styles.h2}>Challenges Faced by Individuals with PCOS</h2>
       <ul>
         <li>Infertility</li>
         <li>Obesity</li>
@@ -55,36 +56,36 @@ const About= () => {
         <li>Eating disorders</li>
       </ul>
 
-      <h2>In-depth Exploration of How PCOS Can Impact Fertility</h2>
-      <p>
+      <h2 className={styles.h2}>In-depth Exploration of How PCOS Can Impact Fertility</h2>
+      <p className={styles.p}>
         PCOS, identified by the World Health Organization as a primary cause of anovulation, significantly contributes to infertility, affecting 70-80% of women with anovulatory infertility. The intricate nature of PCOS, marked by challenges in diagnosis, particularly highlights infertility as a distinctive complication linked to ovarian development.
       </p>
 
-      <p>
+      <p className={styles.p}>
         In a normal ovary, the follicle cycle completes, but in polycystic ovaries, follicle development halts, resulting in multiple immature follicles and contributing to infertility. Hormonal imbalances in PCOS disrupt regular ovulation, causing irregular menstruation, anovulation, and lower-quality eggs, hindering conception.
       </p>
 
-      <p>
+      <p className={styles.p}>
         PCOS-related disorders like insulin resistance and androgen excess further exacerbate infertility issues in affected individuals globally.
       </p>
 
-      <h2>Statistical Information on Fertility Rates for Individuals with PCOS</h2>
+      <h2 className={styles.h2}>Statistical Information on Fertility Rates for Individuals with PCOS</h2>
       <ul>
         <li>7 out of 10 individuals with PCOS are affected by anovulatory infertility</li>
         <li>4.5 times higher likelihood of infertility in women with PCOS.</li>
         <li>70% of cases among 25% of couples experiencing fertility challenges are due to PCOS</li>
       </ul>
 
-      <h2>The Importance of Early Diagnosis</h2>
-      <p>
+      <h2 className={styles.h2}>The Importance of Early Diagnosis</h2>
+      <p className={styles.p}>
         Pre-treatment risk assessment is vital to prevent negative outcomes, and recognizing the higher risk allows for implementing preventive measures to mitigate potential severe outcomes such as spontaneous abortion, the risk of miscarriage, etc.
       </p>
 
-      <p>
+      <p className={styles.p}>
         Thus, analyzing and understanding a PCOS patient's risk of becoming infertile would directly impact the individual’s fertility and reproductive health with relation to PCOS.
       </p>
 
-      <p>
+      <p className={styles.p}>
         Therefore, communicating the risk regarding these possible effects and where appropriate, prevention methods and treatment, should dominate the conversation between the clinician and the patient.
       </p>
 
