c7e4f198f8eb999d75fd48330cbf4e8676c0ba39
Ranuga Anthony <ranuga.20220298@iit.ac.lk>
2024-03-01 23:36:24 +0530

    made more changes to auth service

--- pcos-backend/src/auth/auth.service.ts
+++ pcos-backend/src/auth/auth.service.ts
@@ -15,4 +15,36 @@ export class AuthService {
       if (userDto.password !== userDto.confirmPassword) {
         throw new ValidationError('Password and Confirm Password does not match');
       }
-}
+      const encryptedPassword = await bcrypt.hash(userDto.password, 12);
+      const userId = '1234567890';
+      const token = this.signToken(userId);
+      return { token };
+    }catch (error) {
+        throw new ValidationError(error.message);
+      }
+    }
+
+    async logIn(userDto: any): Promise<{ token: string }> {
+        try {
+          const { email, password } = userDto;
+          if (!email || !password) {
+            throw new ValidationError('Please include email and password.');
+          }
+          const existingUser = { email: 'test@example.com', password: '$2b$12$avRyjUN1JwZLNEz/BiZQQeD79JHmzr5C4OvQpd8wnl0cM50ZmqUJe' };
+          if (!existingUser) {
+            throw new NotFoundException('You are not registered. Please sign up first');
+          }
+    
+          const isPasswordMatches = await bcrypt.compare(password, existingUser.password);
+          if (!isPasswordMatches) {
+            throw new ForbiddenException('Password is incorrect.');
+          }
+          // You would typically retrieve the user ID from the user object
+          const userId = '1234567890';
+          const token = this.signToken(userId);
+            return { token };
+        } catch (error) {
+            //throw new ValidationError(error.message);
+        }
+    }
+
