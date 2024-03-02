a84cdd8f82fd585affca92b67f69562f604a16e8
Ranuga Anthony <ranuga.20220298@iit.ac.lk>
2024-03-02 20:22:07 +0530

    made changes to auth service

--- pcos-backend/src/auth/auth.service.ts
+++ pcos-backend/src/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { ValidationError } from 'src/errors/validation.error';
+import { NotFoundError } from 'src/errors/not-found.error';
+import { ForbiddenError } from 'src/errors/forbidden.error';
 //import * as jwt from 'jsonwebtoken';
 
 @Injectable()
@@ -14,14 +17,14 @@ export class AuthService {
   async signUp(userDto: any): Promise<{ token: string }> {
     try {
       if (userDto.password !== userDto.confirmPassword) {
-        //throw new ValidationError('Password and Confirm Password does not match');
+        throw new ValidationError('Password and Confirm Password does not match');
       }
       const encryptedPassword = await bcrypt.hash(userDto.password, 12);
       const userId = '1234567890';
       const token = this.signToken(userId);
       return { token };
     }catch (error) {
-        //throw new ValidationError(error.message);
+        throw new ValidationError(error.message);
       }
     }
 
@@ -29,23 +32,23 @@ export class AuthService {
         try {
           const { email, password } = userDto;
           if (!email || !password) {
-            //throw new ValidationError('Please include email and password.');
+            throw new ValidationError('Please include email and password.');
           }
           const existingUser = { email: 'test@example.com', password: '$2b$12$avRyjUN1JwZLNEz/BiZQQeD79JHmzr5C4OvQpd8wnl0cM50ZmqUJe' };
           if (!existingUser) {
-            //throw new NotFoundException('You are not registered. Please sign up first');
+            throw new NotFoundError('You are not registered. Please sign up first');
           }
     
           const isPasswordMatches = await bcrypt.compare(password, existingUser.password);
           if (!isPasswordMatches) {
-            //throw new ForbiddenException('Password is incorrect.');
+            throw new ForbiddenError('Password is incorrect.');
           }
           // You would typically retrieve the user ID from the user object
           const userId = '1234567890';
           const token = this.signToken(userId);
             return { token };
         } catch (error) {
-            //throw new ValidationError(error.message);
+            throw new ValidationError(error.message);
         }
     }
 
