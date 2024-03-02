c3e2931ad3b1c694b3cf3704b26a81fea9082eab
Ranuga Anthony <ranuga.20220298@iit.ac.lk>
2024-03-02 20:40:16 +0530

    made changes to auth controller

--- pcos-backend/src/auth/auth.controller.ts
+++ pcos-backend/src/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService : AuthService){}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('signup')
-    async signUp(){
-        try{
-            const token = await this.authService.signUp();
-            return {token};
-        }catch(error)
+  @Post('signup')
+  async signUp() {
+    try {
+      const token = await this.authService.signUp();
+      return { token };
+    } catch (error) {
+      throw new HttpException(
+        'Error while signing up',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Post('login')
+  async logIn(@Body() userDto: any) {
+    try {
+      const token = await this.authService.logIn(userDto);
+      return { token };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error; // If the error is already an HttpException, rethrow it
+      }
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
     }
 }
