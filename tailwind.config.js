2fdaf3d6a619ebcd6e30fb8b66f988589c78d8aa
thevindu20220292 <thevindu.20220292@iit.ac.lk>
2024-03-03 00:35:22 +0530

    tailwind css cofigure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
