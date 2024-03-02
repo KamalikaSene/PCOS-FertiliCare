cf2736afac1052e3155298c0e3a98074e86a86fc
Ranuga Anthony <ranuga.20220298@iit.ac.lk>
2024-03-02 20:49:59 +0530

    made changes to error module

--- pcos-backend/src/errors/errors.module.ts
+++ pcos-backend/src/errors/errors.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { NotFoundError } from './not-found.error';
+import { ValidationError } from './validation.error';
+import { ForbiddenError } from './forbidden.error';
 
-@Module({})
+@Module({
+    providers: [NotFoundError, ValidationError,ForbiddenError],
+    exports: [NotFoundError, ValidationError,ForbiddenError],
+})
 export class ErrorsModule {}
