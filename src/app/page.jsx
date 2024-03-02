8ce299f929b21b7b21c97bd23808ee140c81cdd2
thevindu20220292 <thevindu.20220292@iit.ac.lk>
2024-03-03 00:32:51 +0530

    main page linking

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,32 +4,32 @@ import Link from 'next/link'; // Import Link from next/link
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Link href="/About-PCOS" className={styles.leftTopContainer}  >
+      <Link href="/about-app" className={styles.leftTopContainer}  >
         <img src="/nursepic.jpg" alt="Image 1"  />
         <div>
           {/* Content for the left container */}
           <h2>What is PCOS Fertilicare</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <p>Learn more about us
           </p>
         </div>
       </Link>
 
-      <div className={styles.rightTopContainer}>
+      <Link href="/About-PCOS" className={styles.rightTopContainer}>
         <div>
           {/* Content for the right container */}
           <h2>About PCOS</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+          <p>Gain more knowledge on PCOS  
         </p>
         </div>
         <img src="/aboutpcos.png" alt="Image 2" />
-      </div>
+      </Link>
     
       <div className={styles.leftBottomContainer}>
-        <img src="/predictpic.jpg" alt="Image 1" />
+        <img src="/predictpcos1.jpeg" alt="Image 1" />
         <div>
           {/* Content for the left container */}
           <h2>Predict Infertility Risk Level</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <p>Predict infertility risk level (Restricted for medical personall only)
          
           </p>
         </div>
@@ -39,10 +39,10 @@ export default function Home() {
         <div>
           {/* Content for the right container */}
           <h2>View Past Results</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <p>View the past predicted results
           </p>
         </div>
-        <img src="/infertile2.jpg" alt="Image 2" />
+        <img src="/viewpast1.jpeg" alt="Image 2" />
       </div>
       </div>
   );
