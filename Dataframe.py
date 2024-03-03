272364efe1498c052866a0036e58e67318ffa731
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-03 17:37:23 +0530

    Import necessary libraries

--- Dataframe.py
+++ Dataframe.py
@@ -12,3 +12,4 @@ df.head()
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
