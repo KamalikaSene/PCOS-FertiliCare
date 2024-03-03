9aecf893aa448a7c7b83b4ee268f583f2f85bcc3
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-03 17:30:56 +0530

    Creating a sample dataframes with features

--- Dataframe.py
+++ Dataframe.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import numpy as np
+
+data = {
+    'Feature1': np.random.rand(100),
+    'Feature2': np.random.randint(0, 2, size=100),
+    'Target': np.random.randint(0, 2, size=100)
+}
