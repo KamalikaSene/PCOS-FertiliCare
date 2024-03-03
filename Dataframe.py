ed0fd9cd7e813cf09201facc2200196a5fd0a867
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-03 17:41:15 +0530

    Creating and training the Logistic Regression model

--- Dataframe.py
+++ Dataframe.py
@@ -18,3 +18,6 @@ X = df[['Feature1', 'Feature2']]
 y = df['Target']
 
 X_strain, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
