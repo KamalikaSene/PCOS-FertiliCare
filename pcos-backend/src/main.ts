cf6abe2d9e570a7bee2384c7e94cfdc17e3eeccf
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-06 14:48:00 +0530

    testing

--- pcos-backend/src/main.ts
+++ pcos-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //testing
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
