997bacc2b6f09cca3bca73761d2cfb902dfa0628
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-15 12:52:13 +0530

    connected the page to the queryform page

--- src/app/Predict-PCOS/page.jsx
+++ src/app/Predict-PCOS/page.jsx
@@ -1,4 +1,4 @@
-
+import QueryForm from "@/app/Predict-PCOS/QueryForm";
 
 export const metadata = {
   title: "Predict-Page",
@@ -10,7 +10,12 @@ const Predict = () => {
 
   
   return (
-    <div>hiii byee</div>
+    <div className="p-4 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold">Patient Details Input Query</h1>
+      <p>Fill in the input query below with the patient details.</p>
+    
+      <QueryForm />
+    </div>
   );
 };
 
