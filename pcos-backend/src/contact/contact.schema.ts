267deb31976f749c920b7e6a98273337043e69a2
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-15 11:43:57 +0530

    added the schema and props for each input field

--- pcos-backend/src/contact/contact.schema.ts
+++ pcos-backend/src/contact/contact.schema.ts
@@ -2,6 +2,52 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type ContactDocument = HydratedDocument<Contact>; 
+@Schema()
+export class Contact{
+  @Prop({ required: true, trim: true })
+  pcos: number;
 
+  @Prop({ required: true, trim: true })
+  age: number;
+
+  @Prop({ required: true, trim: true })
+  weight: number;
+
+  @Prop({ required: true, trim: true })
+  height: number;
+
+  @Prop({ required: true, trim: true })
+  bmi: number;
+
+  @Prop({ required: true, trim: true })
+  cycle: number;
+
+  @Prop({ required: true, trim: true })
+  pregnant: number;
+
+  @Prop({ required: true, trim: true })
+  abortion: number;
+
+  @Prop({ required: true, trim: true })
+  fsh: number;
+
+  @Prop({ required: true, trim: true })
+  lh: number;
+
+  @Prop({ required: true, trim: true })
+  fshLhRatio: number;
+
+  @Prop({ required: true, trim: true })
+  prolactin: number;
+
+  @Prop({ required: true, trim: true })
+  weightgain: number;
+
+  @Prop({ required: true, trim: true })
+  fastfood: number;
+
+  @Prop({ required: true, trim: true })
+  exercise: number;
+}
 
 export const ContactSchema = SchemaFactory.createForClass(Contact);
