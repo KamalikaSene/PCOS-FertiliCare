3ff92e07d0b4cc6c77ce17f3437346b652e74540
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-15 11:41:31 +0530

    added all the fields for the DTO file

---
+++ pcos-backend/src/contact/create-contact.dto.ts
@@ -0,0 +1,17 @@
+export class CreateContactDto {
+    readonly pcos: number;
+    readonly age: number;
+    readonly weight: number;
+    readonly height: number;
+    readonly bmi: number;
+    readonly cycle: number;
+    readonly pregnant: number;
+    readonly abortion: number;
+    readonly fsh: number;
+    readonly lh: number;
+    readonly fshLhRatio: number;
+    readonly prolactin: number;
+    readonly weightgain: number;
+    readonly fastfood: number;
+    readonly exercise: number;
+  }
