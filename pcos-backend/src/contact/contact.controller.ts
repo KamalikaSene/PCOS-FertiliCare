ec339d48c31f9d77d7bd9796acb0963caf0bf34c
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-15 11:47:06 +0530

    created the query instance with error handling

--- pcos-backend/src/contact/contact.controller.ts
+++ pcos-backend/src/contact/contact.controller.ts
@@ -7,5 +7,28 @@ import { Response } from 'express';
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
-
+  @Post('contact')
+  async create(
+    @Body() createContactDto: CreateContactDto,
+    @Res() res: Response,
+  ): Promise<any> {
+    try {
+      await this.contactService.create(createContactDto);
+      return res
+        .status(200)
+        .json({ msg: ['Message sent successfully'], success: true });
+    } catch (error) {
+      console.error('Error creating contact:', error);
+      if (error.code !== 11000) {
+        return res.status(500).json({
+          msg: ['Unable to send message.', error.message],
+          success: false,
+        });
+      } else {
+        return res
+          .status(200)
+          .json({ msg: ['Message sent successfully'], success: true });
+      }
+    }
+  }
 }
