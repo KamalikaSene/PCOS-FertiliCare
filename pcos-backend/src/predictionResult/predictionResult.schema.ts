b925a22a8f7399a33b408dee7c5e8b46ad27f99a
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-17 23:38:40 +0530

    modified the schema so it has the other variables bmi,ratio,cycle,prl

--- pcos-backend/src/predictionResult/predictionResult.schema.ts
+++ pcos-backend/src/predictionResult/predictionResult.schema.ts
@@ -6,6 +6,18 @@ export type PredictionResultDocument = HydratedDocument<PredictionResult>;
 @Schema()
 export class PredictionResult {
   @Prop()
+  bmi: number;
+
+  @Prop()
+  fsh_lh: number;
+
+  @Prop()
+  prl_ng_ml: number;
+
+  @Prop()
+  cycle_value: number;
+
+  @Prop()
   prediction: string;
 }
 
