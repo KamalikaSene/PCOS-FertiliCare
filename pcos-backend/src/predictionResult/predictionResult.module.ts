d264e3daa6d008397fbfb7548d952ed6fa293e02
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-17 20:35:46 +0530

    added the schema details in the module file

---
+++ pcos-backend/src/predictionResult/predictionResult.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+//import { PredictionResultController } from './predictionResult.controller';
+import { PredictionResultService } from './predictionResult.service';
+
+import {
+  PredictionResult,
+  PredictionResultSchema,
+} from './predictionResult.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: PredictionResult.name, schema: PredictionResultSchema },
+    ]),
+  ],
+  //controllers: [PredictionResultController],
+  providers: [PredictionResultService],
+})
+export class PredictionResultModule {}
