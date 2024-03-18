e22895b5f64694147f88144b068dcc0e9b0d9fb9
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-18 19:44:08 +0530

    Continue with useeffect functioin, included comments

--- Result page/ResutPage.jsx
+++ Result page/ResutPage.jsx
@@ -11,7 +11,7 @@ import styles from '../styles/resultPage.module.css'; // Import the CSS file
 const ResultPage = () => {
   const [predictionResult, setPredictionResult] = useState(null);
 
-  
+
   useEffect(() => {
     // Function to fetch prediction result from Flask API
     const fetchPredictionResult = async () => {
@@ -20,3 +20,18 @@ const ResultPage = () => {
         const response = await axios.post('http://localhost:5000/predict_infertility_risk', {
           // Pass input data for prediction if needed
         });
+
+         // Extract prediction result from response data
+         const { result } = response.data;
+        
+         // Set the prediction result in state
+         setPredictionResult(result);
+       } catch (error) {
+         console.error('Error fetching prediction result:', error);
+         // Handle error
+       }
+     };
+ 
+     // Call the fetchPredictionResult function
+     fetchPredictionResult();
+   }, []);
