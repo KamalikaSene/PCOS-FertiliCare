deef76391bef6f52af09c4378d1cd533ffebbf19
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-18 19:35:48 +0530

    Necessary imports to communicate with APIs

---
+++ Result page/ResutPage.jsx
@@ -0,0 +1,8 @@
+//fetch the prediction result from the Flask API when the component mounts.
+import { useEffect, useState } from 'react';
+
+//API for performing HTTP requests, making it a common choice for interacting with backend APIs in frontend applications.
+import axios from 'axios';
+
+import Layout from '../components/Layout'; //layout component
+import styles from '../styles/resultPage.module.css'; // Import the CSS file
