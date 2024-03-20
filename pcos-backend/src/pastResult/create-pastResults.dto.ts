f8153c29bb3492b0d1fd31c012bfdc109ce36fcc
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-20 08:39:39 +0530

    Created Past results module files

---
+++ pcos-backend/src/pastResult/create-pastResults.dto.ts
@@ -0,0 +1,7 @@
+export class CreatePredictionResultDto {
+    readonly bmi: number;
+    readonly fsh_lh: number;
+    readonly prl_ng_ml: number;
+    readonly cycle_value: number;
+    readonly risk_level: string;
+  }
