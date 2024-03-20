a6bac6310059416d71fb86bb7b7eabd7902e5f48
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-20 22:31:28 +0530

    removed 4 patient data and added risk level variable

--- pcos-backend/src/predictionResult/create-predictionResult.dto.ts
+++ pcos-backend/src/predictionResult/create-predictionResult.dto.ts
@@ -1,6 +1,3 @@
-export class CreatePatientDataDto {
-  readonly bmi: number;
-  readonly fsh_lh: number;
-  readonly prl_ng_ml: number;
-  readonly cycle_value: number;
+export class CreatePredictionResultDto {
+  readonly risk_level: string;
 }
