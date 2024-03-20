a1893a20c7324a833f8d2b077401f61643559ecd
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-20 22:32:25 +0530

    undid changes to controller file

--- pcos-backend/src/predictionResult/predictionResult.controller.ts
+++ pcos-backend/src/predictionResult/predictionResult.controller.ts
@@ -1,36 +1,21 @@
-import {
-  Controller,
-  Post,
-  Body,
-  Res,
-  Get,
-  Param,
-  NotFoundException,
-} from '@nestjs/common';
-import { PatientDataService } from './patientData.service';
-import { CreatePatientDataDto } from './create-patientData.dto';
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { PredictionResultService } from './predictionResult.service';
+import { CreatePredictionResultDto } from './create-predictionResult.dto';
 import { Response } from 'express';
-// import axios from 'axios';
 
 @Controller('api')
-export class PatientDataController {
-  constructor(private readonly PatientDataService: PatientDataService) {}
+export class PredictionResultController {
+  constructor(
+    private readonly PredictionResultService: PredictionResultService,
+  ) {}
 
-  @Post('patientData')
+  @Post('prediction')
   async create(
-    @Body() createPatientDataDto: CreatePatientDataDto,
+    @Body() createPredictionResultDto: CreatePredictionResultDto,
     @Res() res: Response,
   ): Promise<any> {
     try {
-      await this.PatientDataService.create(createPatientDataDto);
-      // const predictResponse = await axios.post(
-      //   'http://127.0.0.1:5000',
-      //   createPatientDataDto,
-      // );
-
-      // Handle the response from the Flask server if needed
-      // console.log('Prediction from Flask server:', predictResponse.data);
-
+      await this.PredictionResultService.create(createPredictionResultDto);
       return res
         .status(200)
         .json({ msg: ['Message sent successfully'], success: true });
@@ -51,48 +36,25 @@ export class PatientDataController {
       }
     }
   }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string): string {
-  //   return `This action returns a #${id} cat`;
-  // }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string, @Res() res: Response): Promise<any> {
-    try {
-      const patientData = await this.PatientDataService.findOne(id);
-
-      if (!patientData) {
-        throw new NotFoundException('Patient data not found');
-      }
-
-      const { bmi, fsh_lh, prl_ng_ml, cycle_value } = patientData;
-      return res.status(200).json({ bmi, fsh_lh, prl_ng_ml, cycle_value });
-    } catch (error) {
-      console.error('Error retrieving patient data:', error);
-      if (error instanceof NotFoundException) {
-        return res.status(404).json({ msg: error.message, success: false });
-      } else {
-        return res.status(500).json({
-          msg: ['Unable to retrieve patient data.', error.message],
-          success: false,
-        });
-      }
-    }
-  }
 }
 
-// @Post('predict') // Endpoint to call Flask predict
-// async predict(@Body() data: any, @Res() res: Response): Promise<any> {
-//   try {
-//     // Assuming 'data' is the payload you want to send to Flask
-//     const flaskResponse = await axios.post('FLASK_ENDPOINT_URL/predict', data);
-//     return res.status(200).json(flaskResponse.data); // Returning Flask response
-//   } catch (error) {
-//     console.error('Error calling predict endpoint:', error);
-//     return res.status(500).json({
-//       msg: ['Unable to make prediction.', error.message],
-//       success: false,
-//     });
+// import { Controller, Post, Body } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Data } from './data.model';
+
+// @Controller('data')
+// export class DataController {
+//   constructor(@InjectModel('Data') private readonly dataModel: Model<Data>) {}
+
+//   @Post()
+//   async storeData(@Body() formData: any): Promise<any> {
+//     try {
+//       const newData = new this.dataModel(formData);
+//       await newData.save();
+//       return { message: 'Data stored successfully' };
+//     } catch (error) {
+//       return { error: 'Failed to store data' };
+//     }
 //   }
 // }
