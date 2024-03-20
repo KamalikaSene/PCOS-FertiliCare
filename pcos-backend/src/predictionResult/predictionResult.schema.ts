8206c8394744ad97d55b1d182fec63cd373205e5
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-20 22:33:30 +0530

    removed the patient data props

--- pcos-backend/src/predictionResult/predictionResult.schema.ts
+++ pcos-backend/src/predictionResult/predictionResult.schema.ts
@@ -6,18 +6,6 @@ export type PredictionResultDocument = HydratedDocument<PredictionResult>;
 @Schema()
 export class PredictionResult {
   @Prop()
-  bmi: number;
-
-  @Prop()
-  fsh_lh: number;
-
-  @Prop()
-  prl_ng_ml: number;
-
-  @Prop()
-  cycle_value: number;
-
-  @Prop()
   risk_level: string;
 }
 
