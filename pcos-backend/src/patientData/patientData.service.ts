2c112c70e7fa32486014f871465536a04d2ce721
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-20 22:39:51 +0530

    created the patient data service file

---
+++ pcos-backend/src/patientData/patientData.service.ts
@@ -0,0 +1,24 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PatientData } from './patientData.schema';
+import { CreatePatientDataDto } from './create-patientData.dto';
+
+@Injectable()
+export class PatientDataService {
+  constructor(
+    @InjectModel(PatientData.name)
+    private PatientDataModel: Model<PatientData>,
+  ) {}
+
+  async create(
+    createPatientDataDto: CreatePatientDataDto,
+  ): Promise<PatientData> {
+    const createdPatientData = new this.PatientDataModel(createPatientDataDto);
+    return createdPatientData.save();
+  }
+
+  async findOne(id: string): Promise<PatientData | null> {
+    return this.PatientDataModel.findById(id).exec();
+  }
+}
