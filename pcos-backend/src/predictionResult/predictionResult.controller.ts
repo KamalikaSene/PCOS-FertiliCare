c645b4cd145afa709754143da8f53b28f820a566
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-23 11:55:41 +0530

    added the controllers for view result, view past result and delete result

--- pcos-backend/src/predictionResult/predictionResult.controller.ts
+++ pcos-backend/src/predictionResult/predictionResult.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Body, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Res,
+  Get,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
 import { PredictionResultService } from './predictionResult.service';
 import { CreatePredictionResultDto } from './create-predictionResult.dto';
 import { Response } from 'express';
@@ -6,7 +16,7 @@ import { Response } from 'express';
 @Controller('api')
 export class PredictionResultController {
   constructor(
-    private readonly PredictionResultService: PredictionResultService,
+    private readonly predictionResultService: PredictionResultService,
   ) {}
 
   @Post('prediction')
@@ -15,12 +25,11 @@ export class PredictionResultController {
     @Res() res: Response,
   ): Promise<any> {
     try {
-      await this.PredictionResultService.create(createPredictionResultDto);
+      await this.predictionResultService.create(createPredictionResultDto);
       return res
         .status(200)
         .json({ msg: ['Message sent successfully'], success: true });
     } catch (error) {
-      // Handle any errors other than the duplicate email error
       console.error('Error creating contact:', error);
       if (error.code !== 11000) {
         return res.status(500).json({
@@ -28,33 +37,81 @@ export class PredictionResultController {
           success: false,
         });
       } else {
-        // No need to handle the duplicate email error, as we're allowing duplicates
-        // Proceed with successful response
         return res
           .status(200)
           .json({ msg: ['Message sent successfully'], success: true });
       }
     }
   }
+
+  @Get('viewresult')
+  async getLastRecord(@Res() res: Response): Promise<any> {
+    try {
+      const lastRecord = await this.predictionResultService.findLast();
+      if (!lastRecord) {
+        throw new NotFoundException('No records found');
+      }
+      // return res.status(HttpStatus.OK).json(lastRecord);
+      return res
+        .status(HttpStatus.OK)
+        .json({ risk_level: lastRecord.risk_level });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ msg: 'No records found' });
+      }
+      console.error('Error fetching last record:', error);
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ msg: 'Error fetching data' });
+    }
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string, @Res() res: Response): Promise<any> {
+    try {
+      const pastResult = await this.predictionResultService.findOne(id);
+
+      if (!pastResult) {
+        throw new NotFoundException('Past risk level prediction not found');
+      }
+
+      const { risk_level } = pastResult;
+      return res.status(200).json({ risk_level });
+    } catch (error) {
+      console.error('Error retrieving past risk level prediction:', error);
+      if (error instanceof NotFoundException) {
+        return res.status(404).json({ msg: error.message, success: false });
+      } else {
+        return res.status(500).json({
+          msg: [
+            'Unable to retrieve past risk level prediction.',
+            error.message,
+          ],
+          success: false,
+        });
+      }
+    }
+  }
+
+  @Post('deleteresult')
+  async deleteLastRecord(): Promise<any> {
+    try {
+      const deletedRecord =
+        await this.predictionResultService.deleteLastRecord();
+      if (!deletedRecord) {
+        throw new HttpException(
+          'No record found to delete',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return { message: 'Last record deleted successfully' };
+    } catch (error) {
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
-
-// import { Controller, Post, Body } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { Model } from 'mongoose';
-// import { Data } from './data.model';
-
-// @Controller('data')
-// export class DataController {
-//   constructor(@InjectModel('Data') private readonly dataModel: Model<Data>) {}
-
-//   @Post()
-//   async storeData(@Body() formData: any): Promise<any> {
-//     try {
-//       const newData = new this.dataModel(formData);
-//       await newData.save();
-//       return { message: 'Data stored successfully' };
-//     } catch (error) {
-//       return { error: 'Failed to store data' };
-//     }
-//   }
-// }
