994661cca6ec743016574c7c99af5230e5769280
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-23 11:38:35 +0530

    changed code format in doctor auth service

--- pcos-backend/src/doctor-auth/doctor-auth/doctor-auth.service.ts
+++ pcos-backend/src/doctor-auth/doctor-auth/doctor-auth.service.ts
@@ -6,11 +6,16 @@ import { DoctorAuthDto } from './doctor-auth.dto';
 
 @Injectable()
 export class DoctorAuthService {
-  constructor(@InjectModel(DoctorAuth.name) private readonly doctorModel: Model<DoctorAuth>) {}
+  constructor(
+    @InjectModel(DoctorAuth.name)
+    private readonly doctorModel: Model<DoctorAuth>,
+  ) {}
 
   async authenticateDoctor(dto: DoctorAuthDto): Promise<boolean> {
     const { registrationNumber, Surname } = dto;
-    const doctor = await this.doctorModel.findOne({ registrationNumber, Surname }).exec();
+    const doctor = await this.doctorModel
+      .findOne({ registrationNumber, Surname })
+      .exec();
     console.log(doctor);
 
     return !!doctor;
