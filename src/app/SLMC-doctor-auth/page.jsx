88ed3fbca252bc0f3dc1da22f74b01756bfb95fa
thevindu20220292 <thevindu.20220292@iit.ac.lk>
2024-03-23 19:32:36 +0530

    slmc form

--- src/app/SLMC-doctor-auth/page.jsx
+++ src/app/SLMC-doctor-auth/page.jsx
@@ -1,6 +1,6 @@
 
 import SLMCForm from "@/components/slmcForm/SLMCForm";
-import { handleGithubLogin } from "@/lib/action";
+
 import styles from "./auth.module.css";
 
 const Doctorauth = () => {
@@ -8,26 +8,7 @@ const Doctorauth = () => {
   return (
 
 
-    <div class="container my-24 mx-auto md:px-6">
-    <section class="mb-32">
-      <div class="flex flex-wrap">
-        <div class="mb-12 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-5/12">
-          <div class="flex lg:py-12">
-            <img src="/Aboutpcos1.jpeg"
-              class="z-[10] w-full rounded-lg shadow-lg dark:shadow-black/20 lg:ml-[50px]" alt="image" />
-          </div>
-        </div>
-  
-        <div class="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
-          <div
-            class="flex h-full items-center rounded-lg bg-green-200 p-6 text-center text-white lg:pl-12 lg:text-left">
-            <div class="lg:pl-12">
-            
-            
-              <div class="mx-auto mb-8 flex flex-col md:flex-row md:justify-around xl:justify-start">
-                
-               
-              </div>
+    
               <div className={styles.container}>
                 <div className={styles.wrapper}>
                   
@@ -35,13 +16,9 @@ const Doctorauth = () => {
                 </div>
               </div>
               
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+            
 
-  </div>
+
 
 
     
