17da9e50abbf3d158d6a2c5a6e7f045ef890c109
KamalikaSene <148671381+KamalikaSene@users.noreply.github.com>
2024-03-24 21:34:06 +0530

    removed the code which stores the response and the error handling when handling patient data

--- pcos-backend/src/patientData/patientData.controller.ts
+++ pcos-backend/src/patientData/patientData.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Post, Body, Res } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { PatientDataService } from './patientData.service';
 import { CreatePatientDataDto } from './create-patientData.dto';
-// import { Response } from 'express';
 import axios from 'axios';
 
 @Controller('api')
@@ -11,26 +10,7 @@ export class PatientDataController {
   @Post('patientData')
   async create(
     @Body() createPatientDataDto: CreatePatientDataDto,
-    // @Res() res: Response,
   ): Promise<any> {
-    // try {
     await axios.post('http://localhost:5000/predict', createPatientDataDto);
-
-    //   return res
-    //     .status(200)
-    //     .json({ msg: ['Message sent successfully'], success: true });
-    // } catch (error) {
-    //   console.error('Error creating contact:', error);
-    //   if (error.code !== 11000) {
-    //     return res.status(500).json({
-    //       msg: ['Unable to send message.', error.message],
-    //       success: false,
-    //     });
-    //   } else {
-    //     return res
-    //       .status(200)
-    //       .json({ msg: ['Message sent successfully'], success: true });
-    //   }
-    // }
   }
 }
